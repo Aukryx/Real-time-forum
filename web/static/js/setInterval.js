355fb1e3e9c78f309a3d8c6967f212c0f9417ee1
Golden76z <damienprouet76@gmail.com>
2025-03-11 18:25:10 +0100

    Setinterval done

---
+++ web/static/js/setInterval.js
@@ -0,0 +1,25 @@
+import { populatePostList } from "./posts.js";
+import { populateUserList } from "./user_list.js";
+
+// Check for new posts every 10 seconds
+setInterval(async () => {
+    console.log("Interval");
+    
+    try {
+        const response = await fetch('/api/check-session', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            // Include credentials to send cookies
+            credentials: 'include'
+        });
+        
+        const result = await response.json();
+        // If the user is logged, aka on the main page
+        if (result.loggedIn) {
+            populatePostList()
+            populateUserList()
+        }
+    } catch (error) {
+        console.log("Enable to check session (setInterval.js)");
+    }
+}, 10000)
